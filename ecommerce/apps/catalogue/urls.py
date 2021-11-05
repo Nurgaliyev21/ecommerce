4e3837540a100575b6c4ccb2bd600c77ef2babb2
abukajary <jarylgapov.abubakir@gmail.com>
2021-11-06 04:28:40 +0600

    'full-site'

---
+++ ecommerce/apps/catalogue/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "catalogue"
+
+urlpatterns = [
+    path("", views.product_all, name="store_home"),
+    path("full", views.full_shop, name="full_shop"),
+    path("product/<slug:slug>", views.product_detail, name="product_detail"),
+    path("shop/<slug:category_slug>/", views.category_list, name="category_list"),
+]
---
+++ ecommerce/apps/catalogue/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
+
+
+def full_shop(request):
+    products = Product.objects.prefetch_related("product_image").filter(is_active=True)
+    return render(request, "catalogue/all_products.html", {"products": products})
+
+
+def product_all(request):
+    category = Category.objects.all()
+    products = Product.objects.prefetch_related("product_image").filter(is_active=True).order_by('id')
+    return render(request, "catalogue/index.html", {"products": products, "category": category})
+
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category__slug__contains=category.slug)
+    return render(request, "catalogue/category.html", {"category": category, "products": products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, "catalogue/single.html", {"product": product})
+
